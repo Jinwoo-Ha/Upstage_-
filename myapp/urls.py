d6b436351a28b1f3bf7e4c9f5120c0c64a856047
Sweet-potato_00 <92353593+Jinwoo-Ha@users.noreply.github.com>
2024-10-05 04:21:44 +0900

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('end/', views.end, name='end'),
     path('chatbot/', views.chatbot_view, name='chatbot'),
     path('check_story_status/<int:story_id>/', views.check_story_status, name='check_story_status'),
+    #google cloud tts
+    path('tts/', views.text_to_speech, name='text_to_speech'),
+
 ]
